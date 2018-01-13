a4c3f8d442223a7da090ac0aa70225f56ad5776a
Anubhav Saini <anusaini@paypal.com>
2018-01-13 15:58:07 +0530

    Version 3

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const asciiAlphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 const isAscii = ch => ch.charCodeAt(0) <= 127
+
 const encodeChar = ch => isAscii ? String.fromCharCode(ch.charCodeAt(0) + 0x2800) : ch
+
 const encode = (str, separator) => str.split('').map(encodeChar).join(separator || '')
+
 const encodeCharCode = chCode => chCode >= 32 && chCode <= 127 ? String.fromCharCode(chCode + 0x2800) : undefined
-const asciiMap = () => {
+
+const asciiMap = (() => {
   const map = []
   for (let i = 32; i <= 127; i++) {
     map.push({
@@ -13,20 +18,22 @@ const asciiMap = () => {
     })
   }
   return map
-}
+})()
+
 const alphabetMap = asciiAlphabet.split('').map(i => {
   return {
     ascii: i,
     braille: encodeChar(i)
   }
 })
-const alphabetString = encode(asciiAlphabet)
+
+const brailleAlphabet = encode(asciiAlphabet)
 
 module.exports = {
   alphabetMap,
-  alphabetString,
   asciiAlphabet,
   asciiMap,
+  brailleAlphabet,
   encode,
   encodeChar,
   encodeCharCode,
